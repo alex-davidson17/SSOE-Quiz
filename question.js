691268559cb9f78da488d6a95dacb04adeac405e
Alyssa Davidson <alyssa.davidson17@gmail.com>
2020-07-09 23:46:31 -0400

    updated authorship

--- question.js
+++ question.js
@@ -1,4 +1,4 @@
-// code by webdevtrick (https://webdevtrick.com)
+// written by Alex Davidson
 function Quiz(questions) {
     this.score = 0;
     this.questions = questions;
@@ -82,8 +82,8 @@ var questions = [
     new Question("What is the most common cause of dementia in the U.S.?", ["Huntington’s disease", "Parkinson’s disease", "Multiple sclerosis", "Alzheimer’s disease"], "Alzheimer’s disease"),
     new Question("Recycling efforts have attempted to curb the use of plastic, but in the U.S. alone only 30 percent of plastic is recycled. Globally, how many plastic bottles are produced every second?", ["200", "2,000","20,000", "200,000"], "20,000"),
     new Question("Having good tread on your shoes helps prevent slips and falls. It’s time to replace those shoes when the worn patch becomes larger than a WHAT?", ["AA battery", "Quarter", "Tealight candle", "Can of soda"], "AA battery"),
-		new Question("Which of the following daily objects cannot carry viruses and bacteria, such as the novel coronavirus?", ["Your cellphone", "A face mask", "Money", "They can all carry viruses and bacteria!"], "They can all carry viruses and bacteria!"),
-		new Question("Recruited by George Westinghouse, Reginald Fessenden was the first chair of electrical engineering at Pitt and was known as the Father of WHAT?", ["Radio", "Television","The gramophone", "Automated manufacturing"], "Radio")
+	new Question("Which of the following daily objects cannot carry viruses and bacteria, such as the novel coronavirus?", ["Your cellphone", "A face mask", "Money", "They can all carry viruses and bacteria!"], "They can all carry viruses and bacteria!"),
+	new Question("Recruited by George Westinghouse, Reginald Fessenden was the first chair of electrical engineering at Pitt and was known as the Father of WHAT?", ["Radio", "Television","The gramophone", "Automated manufacturing"], "Radio")
 ];
  
 // create quiz
